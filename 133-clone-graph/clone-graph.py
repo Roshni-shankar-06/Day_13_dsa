23a427ffedaf6897b30047649683bc77d1b96754
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-21 11:15:38 +0530

    Update clone-graph.py

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,8 +11,4 @@ class Solution:
         
         newNode = Node(node.val, [])
         self.map[node] = newNode
-        
-        for neighbor in node.neighbors:
-            newNode.neighbors.append(self.cloneGraph(neighbor))
-            
-        return newNode
+    
