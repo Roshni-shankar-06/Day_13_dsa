bbfd1984e3af0f4a2634f09798955cc4d52abc99
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-21 11:19:09 +0530

    Refactor cloneGraph method by removing checks
    Removed initial checks for None and existing node in cloneGraph method.

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -3,9 +3,4 @@ class Solution:
         self.map = {}
 
     def cloneGraph(self, node: 'Node') -> 'Node':
-        if not node:
-            return None
-        
-        if node in self.map:
-     
-      
+    
