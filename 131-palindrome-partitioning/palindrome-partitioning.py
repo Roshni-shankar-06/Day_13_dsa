5f6fc60f84720721e037b85086e97bd155edc3c9
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-21 11:21:31 +0530

    Remove is_palindrome and dfs functions
    Removed the is_palindrome and dfs functions from the partition method.

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,9 +1,4 @@
 class Solution:
     def partition(self, s: str) -> list[list[str]]:
         ans = []
-        
-        def is_palindrome(sub: str) -> bool:
-            return sub == sub[::-1]
-        
-        def dfs(start: int, path: list[str]) -> None:
-           
+      
