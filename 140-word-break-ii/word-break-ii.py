1af648f05a2d0be88ff451b478cfa6d13c22cab0
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-21 11:14:25 +0530

    Refactor wordBreak method by removing inner function
    Removed the inner wordBreak function and its cache decorator.

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,7 +1,3 @@
 class Solution:
   def wordBreak(self, s: str, wordDict: list[str]) -> list[str]:
     wordSet = set(wordDict)
-
-    @functools.lru_cache(None)
-    def wordBreak(s: str) -> list[str]:
-      ans = []
